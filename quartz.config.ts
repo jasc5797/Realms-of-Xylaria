a8550ecd8c628f5bc270b8237573fb467d56cbdf
jasc5797 <jasc5797@gmail.com>
2024-08-26 23:01:42 -0400

    Quartz sync: Aug 26, 2024, 11:01 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["DM", "Templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
